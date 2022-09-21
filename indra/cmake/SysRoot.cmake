59b5fc0d39e5f9e292572648ae88ad46ec751bb4
Nicky <nicky.dasmijn@posteo.nl>
2022-09-21 17:41:03 +0200

    Extract with timestamp.

--- indra/cmake/SysRoot.cmake
+++ indra/cmake/SysRoot.cmake
@@ -8,6 +8,7 @@ if( USE_SYSROOT )
 	  URL https://github.com/slviewer-3p/_build-all/releases/download/v0.7/sysroot.tar.xz
 	  URL_HASH MD5=444c4da625fcc36591702f7c18b2da7a
 	  DOWNLOAD_NO_PROGRESS ON
+	  DOWNLOAD_EXTRACT_TIMESTAMP
 	  )
   endif()
   FetchContent_MakeAvailable( ${SYSROOT_NAME} )

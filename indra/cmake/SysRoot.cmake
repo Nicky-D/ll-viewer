e31a7d0808ae48e89bcb89bd70878af6ed0ae07e
Nicky <nicky.dasmijn@posteo.nl>
2022-09-29 17:19:13 +0200

    Update sysroot

--- indra/cmake/SysRoot.cmake
+++ indra/cmake/SysRoot.cmake
@@ -5,8 +5,8 @@ if( USE_SYSROOT )
   set( SYSROOT_NAME prebuild_files )
   if (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
 	FetchContent_Declare( ${SYSROOT_NAME}
-	  URL https://github.com/slviewer-3p/_build-all/releases/download/v0.8/sysroot.tar.xz
-	  URL_HASH MD5=69682b15fefd6867735a68dc4194624b
+	  URL https://github.com/slviewer-3p/_build-all/releases/download/v0.10/sysroot.tar.xz
+	  URL_HASH MD5=c5fde2e176ef7bd6ed5c366ce842be54
 	  DOWNLOAD_NO_PROGRESS ON
 	  DOWNLOAD_EXTRACT_TIMESTAMP TRUE
 	  )
@@ -16,4 +16,3 @@ if( USE_SYSROOT )
   function (use_prebuilt_binary _binary)
   endfunction()
 endif()
-

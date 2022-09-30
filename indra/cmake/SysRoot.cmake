1c34e21610f4930ce8abcda7b1e5772e4a3e6323
Nicky <nicky.dasmijn@posteo.nl>
2022-09-30 11:54:21 +0200

    Fix DOWNLOAD_EXTRACT_TIMESTAMP for older cmake versions.

--- indra/cmake/SysRoot.cmake
+++ indra/cmake/SysRoot.cmake
@@ -1,14 +1,19 @@
 include_guard()
 include(FetchContent)
 
+# Only available with cmake > 3.24.0
+# sets DOWNLOAD_EXTRACT_TIMESTAMP to FALSE, setting the timestamp of extracted files to time
+# of extraction rather than using the time that is recorded in the archive
+if( POLICY CMP0135)
+    cmake_policy(CMP0135 NEW)
+endif()
+
 if( USE_SYSROOT )
   set( SYSROOT_NAME prebuild_files )
   if (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
 	FetchContent_Declare( ${SYSROOT_NAME}
 	  URL https://github.com/slviewer-3p/_build-all/releases/download/v0.10/sysroot.tar.xz
 	  URL_HASH MD5=c5fde2e176ef7bd6ed5c366ce842be54
-	  DOWNLOAD_NO_PROGRESS ON
-	  DOWNLOAD_EXTRACT_TIMESTAMP TRUE
 	  )
   endif()
   FetchContent_MakeAvailable( ${SYSROOT_NAME} )

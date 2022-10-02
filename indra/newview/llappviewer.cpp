2b574a3c104a5b276bc4e8196709a7d895307bce
Nicky Dasmijn <nicky.dasmijn@posteo.nl>
2022-10-03 00:34:07 +0200

    Linux: Put CEF/Dullahan version into sysinfo

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -130,8 +130,8 @@
 #include "stringize.h"
 #include "llcoros.h"
 #include "llexception.h"
-#if !LL_LINUX
 #include "cef/dullahan_version.h"
+#if !LL_LINUX
 #include "vlc/libvlc_version.h"
 #endif // LL_LINUX
 
@@ -3312,7 +3312,6 @@ LLSD LLAppViewer::getViewerInfo() const
 		info["VOICE_VERSION"] = LLTrans::getString("NotConnected");
 	}
 
-#if !LL_LINUX
 	std::ostringstream cef_ver_codec;
 	cef_ver_codec << "Dullahan: ";
 	cef_ver_codec << DULLAHAN_VERSION_MAJOR;
@@ -3338,9 +3337,6 @@ LLSD LLAppViewer::getViewerInfo() const
 	cef_ver_codec << CHROME_VERSION_PATCH;
 
 	info["LIBCEF_VERSION"] = cef_ver_codec.str();
-#else
-	info["LIBCEF_VERSION"] = "Undefined";
-#endif
 
 #if !LL_LINUX
 	std::ostringstream vlc_ver_codec;
